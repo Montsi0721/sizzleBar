11c195e57341cb2d966e91919c3e5b2db024dbeb
Montsi0721 <montsikhotso@gmail.com>
2026-06-11 18:54:44 +0200

    fru

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,12 @@ app.use('/assets', express.static(assetsDir));
 // Security middleware
 app.use(helmet());
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:5500', 'http://localhost:5500', 'https://sizzle-bar.onrender.com/'],
+    origin: [
+        'http://localhost:3000', 
+        'http://127.0.0.1:5500', 
+        'http://localhost:5500', 
+        'https://sizzle-bar.onrender.com'
+    ],
     credentials: true
 }));
 
