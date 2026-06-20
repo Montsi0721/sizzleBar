388f6026acaac463af3f777826f860dd7e7b55d9
Montsi0721 <montsikhotso@gmail.com>
2026-06-20 15:18:14 +0200

    Update CORS origin from sizzle-bar to top-grill

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use(cors({
         'http://localhost:3000', 
         'http://127.0.0.1:5500', 
         'http://localhost:5500', 
-        'https://sizzle-bar.onrender.com'
+        'https://top-grill.onrender.com'
     ],
     credentials: true
 }));
